da5d1c592d6a44e028473320dfdc22eaa8961601
bindoong01 <mailsky9687@gmail.com>
2026-05-11 10:22:02 +0900

    [FEAT/IT-145] WorkspaceGatewayBindingService 연결 테스트 계약 추가

--- src/main/java/back/domain/gateway/service/WorkspaceGatewayBindingService.java
+++ src/main/java/back/domain/gateway/service/WorkspaceGatewayBindingService.java
@@ -2,11 +2,16 @@ package back.domain.gateway.service;
 
 import back.domain.gateway.client.OpenClawGatewayConnectionContext;
 import back.domain.gateway.dto.request.WorkspaceGatewayBindingReq;
+import back.domain.gateway.dto.request.WorkspaceGatewayConnectionTestReq;
 import back.domain.gateway.dto.response.WorkspaceGatewayBindingRes;
+import back.domain.gateway.dto.response.WorkspaceGatewayConnectionTestRes;
 
 public interface WorkspaceGatewayBindingService {
 
     WorkspaceGatewayBindingRes bindExternalGateway(Long workspaceId, Long memberId, WorkspaceGatewayBindingReq request);
+
+    WorkspaceGatewayConnectionTestRes testExternalGateway(
+            Long workspaceId, Long memberId, WorkspaceGatewayConnectionTestReq request);
 
     OpenClawGatewayConnectionContext getConnectionContext(Long workspaceId);
 }
