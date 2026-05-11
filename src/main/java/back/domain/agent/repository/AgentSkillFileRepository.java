e6221ed6b367509bafae7514c2e3825c217ce0dc
bindoong01 <mailsky9687@gmail.com>
2026-05-11 13:41:39 +0900

    [FEAT/IT-156] Agent 조회 repository 메서드 추가

--- src/main/java/back/domain/agent/repository/AgentSkillFileRepository.java
+++ src/main/java/back/domain/agent/repository/AgentSkillFileRepository.java
@@ -1,7 +1,14 @@
 package back.domain.agent.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import back.domain.agent.entity.AgentSkillFile;
 
-public interface AgentSkillFileRepository extends JpaRepository<AgentSkillFile, Long> {}
+public interface AgentSkillFileRepository extends JpaRepository<AgentSkillFile, Long> {
+
+    List<AgentSkillFile> findByAgentIdOrderByIdAsc(Long agentId);
+
+    List<AgentSkillFile> findByAgentIdInOrderByAgentIdAscIdAsc(List<Long> agentIds);
+}
